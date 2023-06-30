b934336cf453d977c72caa8d060262c5861644ed
udum123 <udum.chizndu123@gmail.com>
2023-06-30 14:09:31 +0100

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// document.getElementById("count").innerText = 5
 
 let countEL = document.getElementById("count-el")
 
 let count = 0
-// console.log(count)
-
-// initialize the count as 0
-// listen for clicks on the increment button
 
 function increment() {
     count = count + 1000
     countEL.innerText = count
     //console.log(count)
 }
+
+// save function 
+function save() {
+    console.log(count)
+}
+
---
+++ welcome/index.js
@@ -0,0 +1,7 @@
+let welcomeEL = document.getElementById('welcome-el')
+
+let name = "chizindu udum"
+
+let greeting = "welcome back...!"
+
+welcomeEL.innerText = greeting + name
